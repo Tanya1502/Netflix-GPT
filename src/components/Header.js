db7245443d8642a424d10fd66e4e338610b3b734
Tanya <tannutanyaverma@gmail.com>
2024-01-18 11:35:02 +0530

    signIn/signUp form & Login Page / Header Page

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+       <img className='w-44' src='https://netflix-googleai.netlify.app/static/media/netflix-header-logo.774e8c1632ac9433608b.png' alt="logo" />
+    </div>
+  )
+}
+
+export default Header
